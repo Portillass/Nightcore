f3e01cdd2267b4f58064677148108932c40c2342
JARELL E. PORTILLAS <115545323+Portillass@users.noreply.github.com>
2025-07-07 21:59:26 +0800

    Update calender.py

--- calender.py
+++ calender.py
@@ -4,5 +4,7 @@ import calendar
 yy = 2023 # year    
 mm = 10 # month
 
+# Print the calendar for the specified month and year
+# The month() function returns a multi-line string with the calendar for that month
 print(calendar.month(yy,mm))
 
